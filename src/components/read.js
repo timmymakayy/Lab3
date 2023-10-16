b255a7f924941c40216499672fd802d9c2ccae7a
timmymakayy <72506816+timmymakayy@users.noreply.github.com>
2023-10-16 15:44:34 +0100

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -3,9 +3,10 @@ import Books from "./Books";
 import axios from "axios";
 function Read(){
 
- 
+ {*useEffect is a React Hook that lets you synchronize a component with an external system.*/}
     const [data, setData] = useState([]);
-
+ {*Add axios to bookapp, axios is a Promise based HTTP client. Using:
+npm install axios */}
     useEffect(()=>{
  axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
         .then(
